bcc394b8cb96029364dc4f89183c0c8effe33dc4
hgspch <hugop92@gmail.com>
2018-08-20 18:03:19 +0200

    01-loading

--- 01-loading/script.js
+++ 01-loading/script.js
@@ -1,1 +1,8 @@
+function showDone() {
+    var prog = document.getElementById("progress");
+    prog.style.display = "none";
+
+    var done = document.getElementById("done");
+    done.style.display = "block";
+}
 console.log("script.js loaded");
